e93cea9e4c48cf1d264627fdd2f5fefd181442dd
Rajmohan Mani <rmani@vibrenthealth.com>
2020-06-11 15:24:25 -0500

    index updated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 function myFunction() {
     alert("Hi")
 }
+
+document.addEventListener("DOMContenLoaded", load);
+
+function load () {  
+    tableau.extensions.initializeAsync().then(() => {
+        const dashboard = tableau.extensions.dashboardContent.dashboard;
+        console.log(dashboard.name);
+    });
+}
